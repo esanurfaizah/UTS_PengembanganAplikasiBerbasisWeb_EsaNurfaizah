09ece4b32a6d457a9ebe270524f8f423199d1a00
esanurfaizah <93923869+esanurfaizah@users.noreply.github.com>
2021-12-10 16:31:28 +0700

    Layouting

---
+++ Footer.php
@@ -0,0 +1,12 @@
+<!-- Footer -->
+<footer>
+      <div class="text-white text-center p-4" style="background-color:#067a15">
+        <p>Copyright &copy; 2021 Al-Hirziyyah. All Right Reserved.</p>
+      </div>
+    <body>
+  </html>  
+    <!-- Akhir Footer -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ Navigation.php
@@ -0,0 +1,39 @@
+   <!-- Navbar -->
+
+   <nav class="navbar navbar-expand-lg navbar-dark bg-success shadow-sm">
+      <div class="container">
+        <a class="navbar-brand" href="#">Pesantren Al-Hirziyyah</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.html">Beranda</a>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Profil
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a class="dropdown-item active" href="sambutan.html">Sambutan Pengasuh Pondok</a></li>
+                  <li><a class="dropdown-item" href="#visi-misi">Visi Misi</a></li>
+                </ul>
+              </li>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Berita.html">Berita</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Prestasi.html">Prestasi</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Galeri.html">Galeri</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Fasilitas.html">Fasilitas</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Akhir Navbar -->
